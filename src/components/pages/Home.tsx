096c37829b778c0d9df8d636c19f5e8d1a188716
pappychamp <ka3.no3pappy@gmail.com>
2024-09-03 21:28:10 +0900

    refactor: api実行関数を別ファイルに分けた

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,26 +5,16 @@ import HomeContents from '../templates/HomeContents.tsx';
 import Eyecatch from '../templates/Eyecatch.tsx';
 import Footer from '../templates/Footer.tsx';
 import { useEffect, useState } from 'react';
-import { getALLTrendData } from '../../api/dataFetcher.ts';
 import { SiteData } from '../../types/trendApi.ts';
 import { currentDate } from '../../utils/date/dateFormatter.ts';
-import { trendDataFormat } from '../../utils/format/dataFormatter.ts';
+import fetchAllTrendData from '../../utils/api/fetchAllTrendData.ts';
 
 const Home = () => {
   const [trend, setTrend] = useState<SiteData | null>(null);
 
   useEffect(() => {
-    const fetchTrendData = async () => {
-      try {
-        const date = currentDate();
-        const trendData = await getALLTrendData(date);
-        const formattedData = trendDataFormat(trendData);
-        setTrend(formattedData);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchTrendData();
+    const date = currentDate();
+    fetchAllTrendData(date, setTrend);
   }, []);
 
   return (
