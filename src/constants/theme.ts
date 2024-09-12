585ad3ab7a63cf5dbf3ca6eff457317c58036d6d
pappychamp <ka3.no3pappy@gmail.com>
2024-09-12 17:37:50 +0900

    refactor: themeを別ファイルに分けた

---
+++ src/constants/theme.ts
@@ -0,0 +1,8 @@
+import { MantineThemeOverride, createTheme } from '@mantine/core';
+
+const theme: MantineThemeOverride = createTheme({
+  fontFamily: 'Kiwi Maru, serif',
+  defaultRadius: 'md',
+});
+
+export default theme;
