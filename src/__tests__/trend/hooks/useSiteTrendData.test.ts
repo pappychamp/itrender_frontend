be5f8dabd4538875c42502503d99468b90ead455
pappychamp <ka3.no3pappy@gmail.com>
2024-09-19 16:45:42 +0900

    mod: image_urlフィールド追加によるテスト変更

--- src/__tests__/trend/hooks/useSiteTrendData.test.ts
+++ src/__tests__/trend/hooks/useSiteTrendData.test.ts
@@ -14,6 +14,7 @@ describe('useSiteTrendDataテスト', () => {
       published_at: '2024-01-01T00:00:00Z',
       url: 'https://test.com',
       embed_html: null,
+      image_url: null,
       tags: [
         {
           name: 'test tag',
