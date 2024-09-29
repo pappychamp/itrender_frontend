f4422267fb63be0245324263cb90993f067117fe
pappychamp <ka3.no3pappy@gmail.com>
2024-09-29 15:00:38 +0900

    add: playwrightテスト

---
+++ src/__tests__/e2e/home.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('link', { name: 'Home' }).click();
+  await expect(page.getByText('最終更新日')).toBeVisible();
+});
