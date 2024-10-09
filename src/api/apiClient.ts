8e3e7a8a23c4e28b227b9538e11123d204eca987
pappychamp <ka3.no3pappy@gmail.com>
2024-10-09 16:34:26 +0900

    mod: 環境変数をまとめて管理

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+import { API_URL } from '../constants/config';
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // APIのベースURL
+  baseURL: API_URL, // APIのベースURL
   //   timeout: 2000, // タイムアウトの設定
   headers: {
     'Content-Type': 'application/json',
