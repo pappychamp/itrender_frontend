802ec51c46c79f23bcdc4ccd251868e0c9b7278f
pappychamp <ka3.no3pappy@gmail.com>
2024-11-04 20:37:54 +0900

    mod: faviconの変更

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { MantineProvider } from '@mantine/core';
@@ -11,9 +11,9 @@ import setupSentry from './utils/logs/sentry.ts';
 setupSentry();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <MantineProvider theme={theme}>
-      <App />
-    </MantineProvider>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <MantineProvider theme={theme}>
+    <App />
+  </MantineProvider>,
+  // </React.StrictMode>,
 );
